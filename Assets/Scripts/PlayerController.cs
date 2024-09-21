60e13fc95c3d6eedaa4cb19b40fbb216bef97887
MineManiac <matheusbarrosr@gmail.com>
2024-09-21 16:45:23 -0300

    tutorial feito

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,17 +10,34 @@ public class PlayerController : MonoBehaviour
     private float movementX;
     private float movementY;
     public float speed = 0;
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent <Rigidbody>();
+        count = 0; 
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     private void FixedUpdate() 
     {
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);  
+    }
+
+     void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+       {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+       }
+        
     }
     
     void OnMove (InputValue movementValue)
@@ -29,5 +47,13 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;  
     }
 
+    void SetCountText() 
+    {
+       countText.text =  "Count: " + count.ToString();
+       if (count >= 8)
+       {
+           winTextObject.SetActive(true);
+       }
+    }
 
 }
